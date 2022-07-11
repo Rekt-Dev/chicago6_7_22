7bb9fa10073319456e7ce00135c32c1b6dd00a9b
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-11 19:16:19 +0300

    ..

--- src/services/getData.tsx
+++ src/services/getData.tsx
@@ -31,12 +31,3 @@ export async function getData() {
     </div>
   );
 }
-/* export function importer() {
-  const link = "https://api.artic.edu/api/v1/artworks?limit=100";
-
-  fetch(link, requestOptions)
-    .then((res) => res.json())
-    .then((data) => (obj = data))
-    .then(() => console.log(`this is obj rendered from importer ${obj}`));
-  console.log();
-} */
