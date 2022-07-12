cfd7efa1baefe9a6ea6d3347de9817841eac279c
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-12 17:17:40 +0300

    ..

--- src/services/getData.tsx
+++ src/services/getData.tsx
@@ -1,30 +1,15 @@
-export let obj: any;
-
 export async function getData() {
-
-
+  let data;
   try {
     const response = await fetch(
       "https://api.artic.edu/api/v1/artworks?limit=20"
     );
-    let data=(await response.json());
+    data = await response.json();
   } catch (e) {
     console.log(e);
-  }  
-      let data1 = data;
-    console.log(`this is data1 ${data1}`);
   }
 
-  useEffect(() => {
-    setJsonData();
-    getData();
-  }, []);
-  return (
-    <div className="">
-      <header className="">
-        {loading && <img src={"./"} className="App-logo" alt="logo" />}
-        {todos && todos.map((todo) => <p key={todo.id}>{todo.title}</p>)}
-      </header>
-    </div>
-  );
+  console.log(data);
+
+  return data;
 }
