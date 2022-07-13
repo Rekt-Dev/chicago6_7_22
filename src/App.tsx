df58821e060d0b4c514a8049886ca7107187aa8e
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-13 22:30:57 +0300

    body

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,19 @@ import { Link } from "react-router-dom";
 import { Card } from "./components/Card";
 import { json } from "./services/json";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { getData } from "./services/getData";
+import { getData, data } from "./services/getData";
 
 export default function App() {
-  console.log(`this is getData ${getData()}`)
+getData()
   return (
     <div>
       <Header />
-      <div className="centered">
-        {json.map((obj) => (
-          <div className="">
-            <Card
-              key={obj.id}
-              artistName={obj.name}
-              title={obj.title}
-              year={obj.year}
-              town={obj.town}
-            />
-          </div>
-        ))}
-      </div>
+      <Body />
+      <div>
       <Link to="/2_10">pages : 2-10 | </Link>
       <Link to="/11_20">pages : 11-20 | </Link>
       <Link to="/21_30">pages : 21-30 | </Link>
       <Link to="/31_40">pages : 31-40 | </Link>
-    </div>
+      </div></div>
   );
 }
